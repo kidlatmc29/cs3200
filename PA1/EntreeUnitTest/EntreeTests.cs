07d1b454be75dee04fb48715ce1c3fcd47ab585d
ozabell <kidlatmc@gmail.com>
2021-09-24 11:30:16 -0700

    creating unit testing for pa1, working on set up

---
+++ PA1/EntreeUnitTest/EntreeTests.cs
@@ -0,0 +1,17 @@
+using PA1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PA1
+{
+    [TestClass]
+    public class EntreeTests
+    {
+        [TestMethod]
+        public void getEntreeName()
+        {
+           //Arrange 
+           //Act
+           //Assert
+        }
+    }
+}
