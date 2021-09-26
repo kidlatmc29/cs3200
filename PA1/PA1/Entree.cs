96c2301de2696de3379e161bb0201cd085da0765
ozabell <kidlatmc@gmail.com>
2021-09-25 19:53:53 -0700

    added gets for Entree members

--- PA1/PA1/Entree.cs
+++ PA1/PA1/Entree.cs
@@ -9,17 +9,28 @@ namespace PA1
     {
         private string name;
         private List<string> ingredients;
-
         private List<uint> nutritionStats;
-        private uint experationDate;
-
-        private bool isRefrigerated;
-        private bool needsRefrigeration;
-        private bool isSpoiled; 
         
-        public Entree(string entreeName)
+        private string experationDate;
+        private bool refrigerated;
+        private bool needsRefrigeration;
+        private bool spoiled; 
+        
+        public Entree(string name)
         {
-            name = entreeName;
+            this.name = name;
+        }
+
+        public Entree(string name, List<string> ingredients, List<uint> nutritionStats)
+        {
+            this.name = name;
+            this.ingredients = ingredients;
+            this.nutritionStats = nutritionStats;
+
+            experationDate = "Jan 2022";
+            refrigerated = true;
+            needsRefrigeration = true;
+            spoiled = false;
         }
 
         // PRE:
@@ -28,6 +39,77 @@ namespace PA1
         {
             return name; 
         }
+
+        public void printIngredients()
+        {
+            Console.WriteLine("Ingredients:\n"); 
+
+        }
+
+        public uint getNumOfServings()
+        {
+            return 1;
+        }
+
+        public uint getCals()
+        {
+            return 0;
+        }
+
+        public uint getTotalFat()
+        {
+            return 0;
+        }
+
+        public uint getSFat()
+        {
+            return 0;
+        }
+
+        public uint getTFat()
+        {
+            return 0;
+        }
+
+        public uint getCholest()
+        {
+            return 0;
+        }
+        public uint getSodium()
+        {
+            return 0;
+        }
+
+        public uint getTotalCarbs()
+        {
+            return 0;
+        }
+
+        public uint getFiber()
+        {
+            return 0;
+        }
+
+        public uint getTotalSugar()
+        {
+            return 0;
+        }
+
+        public uint getProtein()
+        {
+            return 0;
+        }
+
+        public string getExperationDate()
+        {
+            return experationDate;
+        }
+
+        public bool isSpoiled()
+        {
+            return spoiled;
+        }
+
     }
 }
 
