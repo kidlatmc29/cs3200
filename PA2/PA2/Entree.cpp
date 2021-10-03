caf841d0468c2838087831bbaf9845bb8883788c
ozabell <kidlatmc@gmail.com>
2021-10-03 15:20:37 -0700

    working on constr and deconstr for Entree class

--- PA2/PA2/Entree.cpp
+++ PA2/PA2/Entree.cpp
@@ -1,4 +1,38 @@
 // Entree.cpp
 // Isabel Ovalles
 
+#include <iostream>
+#include "Entree.h"
 
+using namespace std;
+
+Entree::Entree(string name, vector<string> *ingredients, vector<string> *nutrionStats,
+	vector<string> *contains, bool needsRefridge, bool refrigerated)
+{
+	this->name = name;
+	this->ingredients = ingredients;
+	this->nutritionStats = nutrionStats;
+	this->contains = contains;
+
+	this->needsRefridge = needsRefridge;
+	this->refrigerated = refrigerated;
+	spoiled = isSpoiled();
+}
+
+Entree::~Entree()
+{
+	ingredients->clear();
+	nutritionStats->clear();
+	contains->clear();
+}
+
+Entree::Entree(const Entree& orginal)
+{
+
+}
+
+
+bool Entree::isSpoiled() 
+{
+
+}
--- PA2/PA2/Entree.h
+++ PA2/PA2/Entree.h
@@ -1,2 +1,35 @@
 // Entree.h 
 // Isabel Ovalles
+
+#include <vector>
+#include <ctime>
+
+class Entree {
+	//Class and Interface invarients: 
+
+	private:
+		string name;
+		vector<string> *ingredients; 
+		vector<string> *nutritionStats;
+		vector<string> *contains;
+
+		string experiationDate; 
+		
+		bool needsRefridge;
+		bool refrigerated; 
+		bool spoiled;
+		
+		
+	public:
+		// constructor
+		Entree(string name, vector<string> *ingredients, vector<string> *nutrionStats, 
+						vector<string> *contains, bool needsRefridge, bool refrigerated);
+
+		// copy constrtor
+		Entree(const Entree& original);
+		
+		// deconstructor
+		~Entree();
+
+		bool isSpoiled();
+};
