f1bbf4cf4dad32260d2ae38af12ac65723ce82d2
ozabell <kidlatmc@gmail.com>
2021-10-09 17:55:21 -0700

    nutrition stuff is squared away, moving on to other fxns

--- P2/P2.cpp
+++ P2/P2.cpp
@@ -6,26 +6,38 @@
 
 using namespace std;
 
+void makeCookieEntree(); 
+
 int main()
 {
     cout << "Welcome to P2" << endl << endl;
-    string cookieName = "Grandma's Chocolate Chip Cookies";
-    string cookieNutr = "2 200 10 4 0 0 125 25 1 12 2";
-
-    string cookieIng = "Enriched flour(bleached wheat flour, niacin, reduced iron, thiamin mononitrate, roboflavin, folic acid)$vegetable shortening (palm and canola oil [with TBHQ to preserve freshness])$ semi-sweet chocolate chips (sugar, chocolate liquor, cocoa butter, dextrose, milk fat, soy lecithin, natural and artificial flavors)$sugar$high fructose corn syrup$dairy product solids$less 2% of$molasses$fructose$modified corn starch$polydextrose$leavening (baking soda, ammonium bicarbonate)$propylene glycol mono- and diesters of fats and fatty acides, mono- and diglycerides$soy lecithin$salt$eggs$caramel color$natural and artificial flavors";
-    string cookieContains = "egg$milk$soy$wheat";
-
-    Entree cookie(cookieName, cookieIng,cookieNutr,cookieContains, "5/1/2028", false, false);
-
-    cout << "Creating an Entree..." << endl;
-
-    cout << "Entree Name: " << cookie.getName() << endl;
-    cout << "Entree Expiration Date: " << cookie.getExpirationDate() << endl;
-    cout << "Entree Nutrition Facts ========================================="
-        << endl;
-    cout << "Num Of Servings: " << cookie.getNumOfServings() << endl;
-
+    makeCookieEntree();
     cout << endl << "End of P2" << endl << endl;
-
-
 }
+
+void makeCookieEntree()
+ {
+   string cookieName = "Grandma's Chocolate Chip Cookies";
+   string cookieNutr = "2 200 10 4 0 0 125 25 1 12 2";
+
+   string cookieIng = "Enriched flour(bleached wheat flour, niacin, reduced iron, thiamin mononitrate, roboflavin, folic acid)$vegetable shortening (palm and canola oil [with TBHQ to preserve freshness])$ semi-sweet chocolate chips (sugar, chocolate liquor, cocoa butter, dextrose, milk fat, soy lecithin, natural and artificial flavors)$sugar$high fructose corn syrup$dairy product solids$less 2% of$molasses$fructose$modified corn starch$polydextrose$leavening (baking soda, ammonium bicarbonate)$propylene glycol mono- and diesters of fats and fatty acides, mono- and diglycerides$soy lecithin$salt$eggs$caramel color$natural and artificial flavors";
+   string cookieContains = "egg$milk$soy$wheat";
+
+   Entree cookie(cookieName, cookieIng,cookieNutr,cookieContains, "5/1/2028", false, false);
+
+   cout << "Creating an Entree..." << endl << endl;
+
+   cout << "Entree Name: " << cookie.getName() << endl;
+   cout << "Entree Expiration Date: " << cookie.getExpirationDate() << endl;
+   cout << "Entree Nutrition Facts ========================================="
+       << endl;
+   cout << "Num Of Servings: " << cookie.getNumOfServings() << endl;
+   cout << "Calories: " << cookie.getCalories() << endl;
+   cout << "Total Fat: " << cookie.getTotalFat() << endl;
+   cout << "Saturated Fat: " << cookie.getSatFat() << endl;
+   cout << "Trans Fat: " << cookie.getTransFat() << endl;
+   cout << "Cholestorol: " << cookie.getCholest() << endl;
+   cout << "Sodium: " << cookie.getSodium() << endl;
+   cout << "Total Sugar: " << cookie.getTotalSugar() << endl;
+   cout << "Protein: " << cookie.getProtein() << endl;
+ }
--- P2/entree.cpp
+++ P2/entree.cpp
@@ -65,8 +65,50 @@ string Entree::getExpirationDate()
 	return expirationDate;
 }
 
+string Entree::getNumOfServings()
+{
+	return nutritionStats[NUM_OF_SERVINGS];
+}
 
+string Entree::getCalories()
+{
+	return nutritionStats[CALORIES];
+}
 
+string Entree::getTotalFat()
+{
+	return nutritionStats[TOTAL_FAT];
+}
+
+string Entree::getSatFat()
+{
+	return nutritionStats[SAT_FAT];
+}
+
+string Entree::getTransFat()
+{
+	return nutritionStats[TRANS_FAT];
+}
+
+string Entree::getCholest()
+{
+	return nutritionStats[CHOLEST];
+}
+
+string Entree::getSodium()
+{
+	return nutritionStats[SODIUM];
+}
+
+string Entree::getTotalSugar()
+{
+	return nutritionStats[TOTAL_SUGAR];
+}
+
+string Entree::getProtein()
+{
+	return nutritionStats[PROTEIN];
+}
 
 void Entree::powerOut()
 {
