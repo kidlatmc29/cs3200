2c1616f9911c559ffe5356dfc8c748c715319c61
ozabell <kidlatmc@gmail.com>
2021-10-12 19:28:05 -0700

    wrote load and resize fxn for vendor

--- P2/vendor.cpp
+++ P2/vendor.cpp
@@ -11,4 +11,62 @@ Vendor::Vendor(string name, bool isRefrigerator)
   stock = new Entree[INITIAL_SIZE];
   quanties = new int[INITIAL_SIZE];
   prices = new double[INITIAL_SIZE];
+
+  totalItems = 0;
+  currentSize = INITIAL_SIZE;
+}
+
+Vendor::~Vendor()
+{
+  delete[] stock;
+  delete[] quanties;
+  delete[] prices;
+
+  stock = nullptr;
+  quanties = nullptr;
+  prices = nullptr;
+}
+
+void Vendor::load(Entree newItem, int qty, double price)
+{
+  if(totalItems + 1 > currentSize){
+    // need to resize
+  } else {
+    stock[totalItems] = newItem;
+    quanties[totalItems] = qty;
+    prices[totalItems] = price;
+  }
+
+void Vendor::resize()
+{
+  int newSize = currentSize * 2;
+
+  Entree *resizeStock = new Entree[newSize];
+  int *resizeQuanties = new int[newSize];
+  double *resizePrices = new double[newSize];
+
+  for(int i = 0; i < currentSize; i++)
+  {
+    resizeStock[i] = stock[i];
+  }
+
+  for(int i = 0; i < currentSize; i++)
+  {
+    resizeQuanties[i] = quanties[i];
+  }
+
+  for(int i = 0; i < currentSize; i++)
+  {
+    resizePrices[i] = prices[i];
+  }
+
+  delete[] stock;
+  delete[] quanties;
+  delete[] prices;
+
+  stock = resizeStock;
+  quanties = resizeQuanties;
+  prices = resizePrices;
+
+  currentSize = newSize; 
 }
--- P2/vendor.h
+++ P2/vendor.h
@@ -4,37 +4,45 @@
 class Vendor
 {
   private:
-  const int INITIAL_SIZE = 10;
-  // members:
-  string name;
-  // some container for Entrees needs to be dynamic, var number
-  // each item in stock has a quanity, price and characteristics of that entree?
-    // possibily make an array corresponding with those attributes?
+    const int INITIAL_SIZE = 10;
+    // members:
+    string name;
+    // some container for Entrees needs to be dynamic, var number
+    // each item in stock has a quanity, price and characteristics of that entree?
+      // possibily make an array corresponding with those attributes?
     // ex.) Oreos qty:10 price: $1.00
-  Entree *stock;
-  int *quanties;
-  double *prices;
+    Entree *stock;
+    int *quanties;
+    double *prices;
 
+    int totalItems;
+    int currentSize;
 
-  // is stored in refridge
-  bool isRefrigerator;
+    // is stored in refridge
+    bool isRefrigerator;
 
-  // behaviors
-  Vendor(string name, bool isRefrigerator);
+  public:
+    // behaviors
+    Vendor(string name = "New_Vendor", bool isRefrigerator = false);
 
-  // load() adds specific num of Entrees
+    ~Vendor();
 
-  // sells sells ONE of the items to a specific customer IF they can pay for it
-    // NO SELLING SPOILED OR EXPIERED Entrees
+    void load(Entree newItem, int qty, double price);
+    // adds specific num of Entrees
 
-  // poweroutage() vendor loses power, and food req refridge is SPOILED
+    // sells sells ONE of the items to a specific customer IF they can pay for it
+      // NO SELLING SPOILED OR EXPIERED Entrees
 
-  // cleanStock() based on current date, items that are expired or spoiled are
-  // removed from stock
+      // poweroutage() vendor loses power, and food req refridge is SPOILED
 
-  // isStocked checks if current item is available for sail and is not spoiled
-  // or expired
+      // cleanStock() based on current date, items that are expired or spoiled are
+      // removed from stock
 
-  // deep copying must be supported
+      // isStocked checks if current item is available for sail and is not spoiled
+      // or expired
 
+      // deep copying must be supported
+
+      // resize arrays
+      void resize();
 };
