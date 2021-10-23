4f8267b3313bb4a3c8e0646542da9f315b4c0c90
ozabell <kidlatmc@gmail.com>
2021-10-23 14:53:06 -0700

    added isExpired test true test

--- p3/P3 Unit Tests/entreeTest.cs
+++ p3/P3 Unit Tests/entreeTest.cs
@@ -1,6 +1,10 @@
 using System; 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+// Arrange
+// Act
+// Assert
+
 namespace p3
 {
     [TestClass]
@@ -9,16 +13,32 @@ namespace p3
         [TestMethod]
         public void Test_getName_entree_true()
         { 
-            //Arrange
+            // Arrange
             string txt = "Grandma's Chocolate Chip Cookies	2	200	10	4	0	0	125	25	1	12	2	\"Enriched flour(bleached wheat flour, niacin, reduced iron, thiamin mononitrate, roboflavin, folic acid)$vegetable shortening (palm and canola oil [with TBHQ to preserve freshness])$ semi-sweet chocolate chips (sugar, chocolate liquor, cocoa butter, dextrose, milk fat, soy lecithin, natural and artificial flavors)$sugar$high fructose corn syrup$dairy product solids$less 2% of$molasses$fructose$modified corn starch$polydextrose$leavening (baking soda, ammonium bicarbonate)$propylene glycol mono- and diesters of fats and fatty acides, mono- and diglycerides$soy lecithin$salt$eggs$caramel color$natural and artificial flavors\"	egg$milk$soy$wheat";
             DateTime exp = new DateTime(2022,5,1);
             Entree obj = new Entree(txt, exp, false, false);
             string expected = "Grandma's Chocolate Chip Cookies";
-            //Act
+           
+            // Act
             string actual = obj.getName(); 
 
             // Assert
             Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Test_isExpired_entree_true()
+        {
+            // Arrange
+            string txt = "Grandma's Chocolate Chip Cookies	2	200	10	4	0	0	125	25	1	12	2	\"Enriched flour(bleached wheat flour, niacin, reduced iron, thiamin mononitrate, roboflavin, folic acid)$vegetable shortening (palm and canola oil [with TBHQ to preserve freshness])$ semi-sweet chocolate chips (sugar, chocolate liquor, cocoa butter, dextrose, milk fat, soy lecithin, natural and artificial flavors)$sugar$high fructose corn syrup$dairy product solids$less 2% of$molasses$fructose$modified corn starch$polydextrose$leavening (baking soda, ammonium bicarbonate)$propylene glycol mono- and diesters of fats and fatty acides, mono- and diglycerides$soy lecithin$salt$eggs$caramel color$natural and artificial flavors\"	egg$milk$soy$wheat";
+            DateTime exp = new DateTime(2021, 10, 10);
+            Entree obj = new Entree(txt, exp, false, false);
+
+            // Act
+            bool actual = obj.isExpired();
+
+            // Assert
+            Assert.AreEqual(actual, true);
         }
     }
 }
