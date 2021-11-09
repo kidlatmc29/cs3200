b9d5a28acce5aa5bca90a659349a3f000100bdf1
ozabell <kidlatmc@gmail.com>
2021-11-08 19:48:58 -0800

    files from p2 do compile and work properly, revising p4.cpp

---
+++ p4/p4.cpp
@@ -0,0 +1,18 @@
+// p4.cpp
+// Isabel Ovalles
+
+#include <iostream>
+#include <vector>
+#include <memory>
+#include "vendor.h"
+#include "customer.h"
+
+using namespace std;
+
+int main()
+{
+  cout << endl << "Welcome to P4" << endl << endl;
+
+  cout << endl << "End of P4" << endl << endl;
+  return 0;
+}
