7a34271e7a063ffdfa9e61ddfb55cefb7cdeab2d
ozabell <kidlatmc@gmail.com>
2021-11-10 13:27:18 -0800

    wrote loadvendor fxn in main

--- p4/p4.cpp
+++ p4/p4.cpp
@@ -14,17 +14,17 @@ using namespace std;
 
 void createCustomers(vector<Customer*> &clients);
 void printCustomers(vector<Customer*> clients);
-void loadingVendor(shared_ptr<Vendor> market);
+void loadingVendor(shared_ptr<Vendor> store);
 void allCustomersBuyOneAppleSlices(vector<Customer*> clients);
 
 int main()
 {
-  shared_ptr<Vendor> bakery;
-  vector<Customer*> clients;
+  shared_ptr<Vendor> store(new Vendor("The Corner Store", true));
+  vector<Customer*> shoppers;
   cout << endl << "Welcome to P4" << endl << endl;
 
-  createCustomers(clients);
-  printCustomers(clients);
+  createCustomers(shoppers);
+  printCustomers(shoppers);
 
   cout << endl << "End of P4" << endl << endl;
   return 0;
@@ -57,9 +57,57 @@ void createCustomers(vector<Customer*> &clients)
   clients.push_back(ali);
 }
 
-void loadingVendor(shared_ptr<Vendor> market)
+void loadingVendor(shared_ptr<Vendor> store)
 {
-  
+  string soda1Name = "Coca-Cola";
+  string soda1Nutr = "1 240 0 0 0 0 75 65 0 65 0";
+  string soda1Ing = "CARBONATED WATER$HIGH FRUCTOSE CORN SYRUP$CARAMEL COLOR$PHOSPHORIC ACID$NATURAL FLAVORS$CAFFEINE";
+  string soda1Contains = "";
+  Entree soda1(soda1Name, soda1Ing, soda1Nutr, soda1Contains, "12/20/20", true, true);
+
+  string soda2Name = "Sprite";
+  string soda2Nutr = "1 140 0 0 0 0 75 65 0 65 0";
+  string soda2Ing = "CARBONATED WATER$HIGH FRUCTOSE CORN SYRUP$CITRIC ACID$NATURAL FLAVORS$SODIUM CITRATE$SODIUM BENZOATE ";
+  string soda2Contains = "";
+  Entree soda2(soda2Name, soda2Ing, soda2Nutr, soda2Contains, "12/20/19", true, true);
+
+  string soda3Name = "Fanta";
+  string soda3Nutr = "1 270 0 0 0 0 75 73 0 73 0";
+  string soda3Ing = "CARBONATED WATER$HIGH FRUCTOSE CORN SYRUP$CITRIC ACID$SODIUM BENZOATE (TO PROTECT TASTE)$NATURAL FLAVORS$MODIFIED FOOD STARCH$SODIUM POLYPHOSPHATES$GLYCEROL ESTER OF ROSIN$YELLOW 6$RED 40";
+  string soda3Contains = "";
+  Entree soda3(soda3Name, soda3Ing, soda3Nutr, soda3Contains, "12/20/21", true, true);
+
+  string cookieName = "Grandma's Chocolate Chip Cookies";
+  string cookieNutr = "2 200 10 4 0 0 125 25 1 12 2";
+  string cookieIng = "Enriched flour(bleached wheat flour, niacin, reduced iron, thiamin mononitrate, roboflavin, folic acid)$vegetable shortening (palm and canola oil [with TBHQ to preserve freshness])$ semi-sweet chocolate chips (sugar, chocolate liquor, cocoa butter, dextrose, milk fat, soy lecithin, natural and artificial flavors)$sugar$high fructose corn syrup$dairy product solids$less 2% of$molasses$fructose$modified corn starch$polydextrose$leavening (baking soda, ammonium bicarbonate)$propylene glycol mono- and diesters of fats and fatty acides, mono- and diglycerides$soy lecithin$salt$eggs$caramel color$natural and artificial flavors";
+  string cookieContains = "egg$milk$soy$wheat";
+  Entree cookie(cookieName, cookieIng,cookieNutr,cookieContains, "10/10/21", false, false);
+
+  string chipsName = "Lay's Classic Potato Chips";
+  string chipsNutr = "2 160 10 2 0 0 170 15 1 0 2";
+  string chipsIng = "Potatoes$Vegetable Oil(Sunflower, Corn and/or Canola Oil)$Salt";
+  string chipsContains = "";
+  Entree chips(chipsName, chipsIng, chipsNutr, chipsContains, "12/1/20", false, false);
+
+  string nutsName = "Planters Nuts on the Go Salted Peanuts";
+  string nutsNutr = "1 170 14 2 0 0 95 5 2 1 7";
+  string nutsIng = "Peanuts$Peanut and/or Cottonseed oil$sea salt";
+  string nutsContains ="peanuts";
+  Entree nuts(nutsName, nutsIng, nutsNutr, nutsContains, "11/31/22", false, false);
+
+  string popName = "Skinny Pop Popcorn";
+  string popNutr = "1	100	6	0.5	0	9	45	9	9	0	2";
+  string popIng = "Popcorn$Sunflower oil$salt";
+  string popContains = "";
+  Entree popcorn(popName, popIng, popNutr, popContains, "4/10/22", false, false);
+
+  store->load(soda1, 3, 1.00);
+  store->load(soda2, 5, 1.40)
+  store->load(soda3, 12, .99);
+  store->load(cookie, 1.50);
+  store->load(chips, 14, 2.50);
+  store->load(nuts, 35, .50);
+  store->load(popcorn, 8, 3.45);
 }
 
 void printCustomers(vector<Customer*> clients)
