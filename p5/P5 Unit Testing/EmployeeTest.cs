63cc03b5bacbede6d39e54521957fae474dcc0e1
ozabell <kidlatmc@gmail.com>
2021-11-26 20:42:20 -0800

    finished unit tests for current employee class

--- p5/P5 Unit Testing/EmployeeTest.cs
+++ p5/P5 Unit Testing/EmployeeTest.cs
@@ -61,5 +61,73 @@ namespace p5
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Test_getFName_defaultCtor_employee_true()
+        {
+            string expected = "N/A";
+            Employee marnie = new Employee();
+
+            string actual = marnie.getFName();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_getFName_employee_true()
+        {
+            string expected = "Marnie";
+            Employee marnie = new Employee(expected, "Silver", 1, "Pokemart"); ;
+
+            string actual = marnie.getFName();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_getLName_defaultCtor_employee_true()
+        {
+            string expected = "N/A";
+            Employee kass = new Employee();
+
+            string actual = kass.getFName();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_getLName_employee_true()
+        {
+            string expected = "Momokoto";
+            Employee kass = new Employee("Kass", expected, 1, "Maruta");
+
+            string actual = kass.getLName();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_setFName_employee_true()
+        {
+            string expected = "Link";
+            Employee link = new Employee();
+            link.setFName(expected);
+
+            string actual = link.getFName();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_setLName_employee_true()
+        {
+            string expected = "Link";
+            Employee link = new Employee();
+            link.setLName(expected);
+
+            string actual = link.getLName();
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
